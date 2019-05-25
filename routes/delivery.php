51d69edd8b8a065a6179c223a877d368c92b9b5a
= <=>
2019-05-26 01:19:38 +0300

    =edit delivert route file

--- routes/delivery.php
+++ routes/delivery.php
@@ -2,7 +2,7 @@
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+ Web Routes
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
