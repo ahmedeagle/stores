7a6ae462054a50566d0fca0a56ebd5129235bf0e
= <=>
2019-08-25 13:12:44 +0300

    =updated

--- resources/views/cpanel/home.blade.php
+++ resources/views/cpanel/home.blade.php
@@ -97,7 +97,7 @@
                     </div><!--End col-md-4-->
                     <div class="col-md-4">
                         <div class="counter">
-                           <a href="{{url('products')}}">
+                           <a href="{{url('products/list')}}">
                             <div class="counter-icon">
                                 <i class="fa fa-product-hunt"></i>
                             </div>
