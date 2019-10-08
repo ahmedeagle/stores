4d7cdec687a47de59a9decd2e82de09015f06064
= <=>
2019-10-08 14:43:39 +0300

    =

--- resources/views/cpanel/providers/providers.blade.php
+++ resources/views/cpanel/providers/providers.blade.php
@@ -42,7 +42,7 @@
                                 <th> رقم الهاتف</th>
                                  <th> الدولة</th>
                                 <th> المدينة </th>
-                                 <th> الحالة </th>
+                                 <th> الحالة   </th>
                                 <th></th>
                             </tr>
                         </thead>
@@ -55,7 +55,7 @@
                                         <td> {{ $provider->country_code.$provider->phone }} </td>
                                          <td> {{ $provider->country }} </td>
                                         <td> {{ $provider->city }} </td>
-                                         <td> {{ ($provider->status == 0)? 'not activated' : 'activated' }} </td>
+                                         <td> {{ ($provider->  publish  == 0)? 'not activated' : 'activated' }} </td>
                                         <td>
                                             <!-- <button class="custom-btn green-bc">
                                                 <i class="fa fa-eye"></i>
