92e96a183850bcc7d2deca48d66ce31aebc88d39
mahmoud.elzoheiry@wisyst.com <mahmoud.elzoheiry@wisyst.com>
2019-12-29 11:39:15 +0200

    save

--- routes/user.php
+++ routes/user.php
@@ -21,164 +21,164 @@ Route::get('test2', function () {
 Route::group(['middleware' => ['api_auth'], 'prefix' => 'api'], function () {
 
 
-	Route::post('/test', [
-		'uses' => 'UserController@test',
-		'as' => 'test'
-	]);
+    Route::post('/test', [
+        'uses' => 'UserController@test',
+        'as' => 'test'
+    ]);
 
-	Route::post('/userSignUp', [
-		'uses' => 'UserController@userSignUp',
-		'as' => 'userSingUp'
-	]);
+    Route::post('/userSignUp', [
+        'uses' => 'UserController@userSignUp',
+        'as' => 'userSingUp'
+    ]);
 
-	Route::get('/userSignUp', [
-		'uses' => 'UserController@echoEmpty',
-		'as' => 'userSingUpGet'
-	]);
-
-
-	Route::post('/userActivation', [
-		'uses' => 'UserController@activateUserAccount',
-		'as' => 'userActivation'
-	]);
-
-	Route::get('/userActivation', [
-		'uses' => 'UserController@echoEmpty',
-		'as' => 'userActivationGet'
-	]);
-
-	Route::post('/resendUserActivationCode', 'UserController@resendActivationCode');
-
-	Route::get('/resendUserActivationCode', 'UserController@echoEmpty');
+    Route::get('/userSignUp', [
+        'uses' => 'UserController@echoEmpty',
+        'as' => 'userSingUpGet'
+    ]);
 
 
-	Route::post('/userLogin', [
-		'uses' => 'UserController@userLogin',
-		'as' => 'userLogin'
-	]);
+    Route::post('/userActivation', [
+        'uses' => 'UserController@activateUserAccount',
+        'as' => 'userActivation'
+    ]);
 
-	Route::get('/userLogin', [
-		'uses' => 'UserController@echoEmpty',
-		'as' => 'userLoginGet'
-	]);
+    Route::get('/userActivation', [
+        'uses' => 'UserController@echoEmpty',
+        'as' => 'userActivationGet'
+    ]);
 
-	Route::post('/userForgetPassword', 'UserController@forgetPassword');
-	Route::get('/userForgetPassword', 'UserController@echoEmpty');
+    Route::post('/resendUserActivationCode', 'UserController@resendActivationCode');
 
-	Route::post('/userUpdatePassword', 'UserController@updatePassword');
-	Route::get('/userUpdatePassword', 'UserController@echoEmpty');
-
-	Route::post('/userResetPassword', 'UserController@resetPassword');
-	Route::get('/userResetPassword', 'UserController@echoEmpty');
-
-	Route::post('/mainCats', 'UserController@mainCats');
-	Route::get('/mainCats', 'UserController@echoEmpty');
-
-	Route::post('/GetCategoryProviders', 'UserController@get_nearest_providers_inside_main_sub_categories');
-	Route::get('/GetCategoryProviders', 'UserController@echoEmpty');
-
-	Route::post('/filterStores', 'UserController@filterStores');
-	Route::get('/filterStores', 'UserController@echoEmpty');
+    Route::get('/resendUserActivationCode', 'UserController@echoEmpty');
 
 
-	Route::post('/getProviderPage', 'UserController@prepareProviderPage');
+    Route::post('/userLogin', [
+        'uses' => 'UserController@userLogin',
+        'as' => 'userLogin'
+    ]);
 
-	Route::get('/getProviderPage', 'UserController@echoEmpty');
+    Route::get('/userLogin', [
+        'uses' => 'UserController@echoEmpty',
+        'as' => 'userLoginGet'
+    ]);
 
-	Route::post('/getProductDetails', 'UserController@getProductDetails');
+    Route::post('/userForgetPassword', 'UserController@forgetPassword');
+    Route::get('/userForgetPassword', 'UserController@echoEmpty');
 
-	Route::get('/getProductDetails', 'UserController@echoEmpty');
+    Route::post('/userUpdatePassword', 'UserController@updatePassword');
+    Route::get('/userUpdatePassword', 'UserController@echoEmpty');
 
+    Route::post('/userResetPassword', 'UserController@resetPassword');
+    Route::get('/userResetPassword', 'UserController@echoEmpty');
 
-	Route::post('/productLikeAndDislike', 'UserController@like_product');
+    Route::post('/mainCats', 'UserController@mainCats');
+    Route::get('/mainCats', 'UserController@echoEmpty');
 
-	Route::get('/productLikeAndDislike', 'UserController@echoEmpty');
+    Route::post('/GetCategoryProviders', 'UserController@get_nearest_providers_inside_main_sub_categories');
+    Route::get('/GetCategoryProviders', 'UserController@echoEmpty');
 
-	Route::post('/getUserFavorites', 'UserController@getUserFavorites');
-
-	Route::get('/getUserFavorites', 'UserController@echoEmpty');
-
-	Route::post('/addComment', 'UserController@addComment');
-
-	Route::get('/addComment', 'UserController@echoEmpty');
-
-	Route::post('/addAddress', 'UserController@addAdress');
-
-	Route::get('/addAddress', 'UserController@echoEmpty');
-
-	Route::post('/getUserAddress', 'UserController@getUserAddresses');
-
-	Route::get('/getUserAddress', 'UserController@echoEmpty');
-
-	Route::post('/deleteUserAddress', 'UserController@deleteUserAddress');
-
-	Route::get('/deleteUserAddress', 'UserController@echoEmpty');
-
-	Route::post('/editProfile', 'UserController@getProfileData');
-
-	Route::get('/editProfile', 'UserController@echoEmpty');
-
-	Route::post('/updateProfile', 'UserController@UpdateProfile');
-
-	Route::get('/editProfile', 'UserController@echoEmpty');
-
-	Route::post('/getOffers', 'UserController@get_offers');
-	Route::get('/getOffers', 'UserController@echoEmpty');
+    Route::post('/filterStores', 'UserController@filterStores');
+    Route::get('/filterStores', 'UserController@echoEmpty');
 
 
-	Route::post('/getJobs', 'UserController@get_Jobs');
-	Route::get('/getJobs', 'UserController@echoEmpty');
+    Route::post('/getProviderPage', 'UserController@prepareProviderPage');
+
+    Route::get('/getProviderPage', 'UserController@echoEmpty');
+
+    Route::post('/getProductDetails', 'UserController@getProductDetails');
+
+    Route::get('/getProductDetails', 'UserController@echoEmpty');
 
 
-	Route::post('/applyToJob', 'UserController@applyJob');
-	Route::get('/applyToJob', 'UserController@echoEmpty');
+    Route::post('/productLikeAndDislike', 'UserController@like_product');
+
+    Route::get('/productLikeAndDislike', 'UserController@echoEmpty');
+
+    Route::post('/getUserFavorites', 'UserController@getUserFavorites');
+
+    Route::get('/getUserFavorites', 'UserController@echoEmpty');
+
+    Route::post('/addComment', 'UserController@addComment');
+
+    Route::get('/addComment', 'UserController@echoEmpty');
+
+    Route::post('/addAddress', 'UserController@addAdress');
+
+    Route::get('/addAddress', 'UserController@echoEmpty');
+
+    Route::post('/getUserAddress', 'UserController@getUserAddresses');
+
+    Route::get('/getUserAddress', 'UserController@echoEmpty');
+
+    Route::post('/deleteUserAddress', 'UserController@deleteUserAddress');
+
+    Route::get('/deleteUserAddress', 'UserController@echoEmpty');
+
+    Route::post('/editProfile', 'UserController@getProfileData');
+
+    Route::get('/editProfile', 'UserController@echoEmpty');
+
+    Route::post('/updateProfile', 'UserController@UpdateProfile');
+
+    Route::get('/editProfile', 'UserController@echoEmpty');
+
+    Route::post('/getOffers', 'UserController@get_offers');
+    Route::get('/getOffers', 'UserController@echoEmpty');
 
 
-	Route::post('/search', 'UserController@search');
-	Route::get('/search', 'UserController@echoEmpty');
-
-	Route::post('/prepareSearch', 'UserController@prepareSearch');
-	Route::get('/prepareSearch', 'UserController@echoEmpty');
-
-	Route::post('/addOrder', 'UserController@addOrder');
-	Route::get('/addOrder', 'UserController@echoEmpty');
-
-	Route::post('/GetOrders', 'UserController@get_list_of_orders');
-	Route::get('/GetOrders', 'UserController@echoEmpty');
+    Route::post('/getJobs', 'UserController@get_Jobs');
+    Route::get('/getJobs', 'UserController@echoEmpty');
 
 
-	Route::post('/orderDetails', 'UserController@getOrderDetails');
-	Route::get('/orderDetails', 'UserController@echoEmpty');
-
-	Route::post('/CancelOrder', 'UserController@cancel_order');
-	Route::get('/CancelOrder', 'UserController@echoEmpty');
-
-	Route::post('/provider_evaluate', 'UserController@provider_evaluate');
-	Route::get('/provider_evaluate', 'UserController@echoEmpty');
+    Route::post('/applyToJob', 'UserController@applyJob');
+    Route::get('/applyToJob', 'UserController@echoEmpty');
 
 
-	Route::post('/delivery_evaluate', 'UserController@delivery_evaluate');
-	Route::get('/delivery_evaluate', 'UserController@echoEmpty');
+    Route::post('/search', 'UserController@search');
+    Route::get('/search', 'UserController@echoEmpty');
 
-	Route::post('/delivery_Methods', 'UserController@get_delivery_Methods');
-	Route::get('/delivery_Methods', 'UserController@echoEmpty');
+    Route::post('/prepareSearch', 'UserController@prepareSearch');
+    Route::get('/prepareSearch', 'UserController@echoEmpty');
 
-	Route::post('/prepareAddOrder', 'UserController@prepareAddOrder');
-	Route::get('/prepareAddOrder', 'UserController@echoEmpty');
+    Route::post('/addOrder', 'UserController@addOrder');
+    Route::get('/addOrder', 'UserController@echoEmpty');
 
-	Route::post('/get_checkout_id', 'UserController@get_checkout_id');
-	Route::get('/get_checkout_id', 'UserController@echoEmpty');
-
-
-	Route::post('/check_payment_status', 'UserController@check_status');
-	Route::get('/get_checkout_id', 'UserController@echoEmpty');
+    Route::post('/GetOrders', 'UserController@get_list_of_orders');
+    Route::get('/GetOrders', 'UserController@echoEmpty');
 
 
-	// Route::post('/getUserBalances', 'UserController@get_user_balance');
-	Route::post('/getUserBalances', 'UserController@getUserBalance');
-	Route::get('/getUserBalances', 'UserController@echoEmpty');
+    Route::post('/orderDetails', 'UserController@getOrderDetails');
+    Route::get('/orderDetails', 'UserController@echoEmpty');
 
-	Route::post('/getUserInvitationCode', 'UserController@getUserInvitationCode');
-	Route::get('/getUserInvitationCode', 'UserController@echoEmpty');
+    Route::post('/CancelOrder', 'UserController@cancel_order');
+    Route::get('/CancelOrder', 'UserController@echoEmpty');
+
+    Route::post('/provider_evaluate', 'UserController@provider_evaluate');
+    Route::get('/provider_evaluate', 'UserController@echoEmpty');
+
+
+    Route::post('/delivery_evaluate', 'UserController@delivery_evaluate');
+    Route::get('/delivery_evaluate', 'UserController@echoEmpty');
+
+    Route::post('/delivery_Methods', 'UserController@get_delivery_Methods');
+    Route::get('/delivery_Methods', 'UserController@echoEmpty');
+
+    Route::post('/prepareAddOrder', 'UserController@prepareAddOrder');
+    Route::get('/prepareAddOrder', 'UserController@echoEmpty');
+
+    Route::post('/get_checkout_id', 'UserController@get_checkout_id');
+    Route::get('/get_checkout_id', 'UserController@echoEmpty');
+
+
+    Route::post('/check_payment_status', 'UserController@check_status');
+    Route::get('/get_checkout_id', 'UserController@echoEmpty');
+
+
+    // Route::post('/getUserBalances', 'UserController@get_user_balance');
+    Route::post('/getUserBalances', 'UserController@getUserBalance');
+    Route::get('/getUserBalances', 'UserController@echoEmpty');
+
+    Route::post('/getUserInvitationCode', 'UserController@getUserInvitationCode');
+    Route::get('/getUserInvitationCode', 'UserController@echoEmpty');
 
 });
